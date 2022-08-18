434dcc5ad3241e0070b781fa82e68d37f62254fe
“Alex <alexfitzgerald90@gmail.com>
2022-08-18 12:52:21 -0400

    adding streamlit

---
+++ streamlit/pages/2_Date_Summary.py
@@ -0,0 +1,13 @@
+import streamlit as streamlit
+import pandas as pd
+import matplotlib.pyplot as plt 
+
+st.title("Data Summary")
+#Load data
+@st.cache
+def load_data(path):
+    pen_data = pd.read_csv(path, index_col=0)
+    return pen_data
+
+penguins = load_data('.Data/penguins.csv')
+
